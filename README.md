b1cca8727c951dc00a82996539cfe72ad83ab925
Brian3647 <victoris3647@gmail.com>
2023-11-11 13:57:12 +0100

    :sparkles: Conv64 0.1.0

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
-# b64id
+# conv64
 
-Decimal to Base64 converter & generator of random Base64 numbers.
+Easily (& blazingly fast) convert base 10 numbers to base 64.
 
-Useful for creating IDs for your data.
+**IMPORTANT NOTE: This is base 64 as in the number base, if you're searching for base64 encryption, use [this](https://crates.io/crates/base64)**
 
-## Usage
-
-Add this to your `Cargo.toml`:
+## Quickstart
 
 ```toml
-[dependencies.b64id]
-git = "https://github.com/Brian3647/b64id"
+# Cargo.toml
+[dependencies]
+conv64 = "*"
 ```
+
+```rust
+// main.rs
+fn main() {
+    let x = conv64::encode(1234567890);
+    println!("{}", x); // 1LY7VK
+}
+```
+
+## Uses
+
+The main application for this is using it as an ID generator/shortener (for example, YouTube video IDs are base 64 encoded numbers).
+
+Since its main use is for the web, the 2 extra characters (`+` and `/`) are replaced with `-` and `_` respectively, so it can be used in URLs without encoding.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- src/lib.rs
+++ src/lib.rs
@@ -1,103 +1,104 @@
-//! Utility for working with IDs using Base64
-//!
-//! # Why base64 for IDs?
-//!
-//! I recommend taking a look to this Tom scott video: https://www.youtube.com/watch?v=gocwRvLhDf8
-
-#![deny(unsafe_code)]
-#![warn(missing_docs)]
+#![forbid(unsafe_code)]
+#![forbid(missing_docs)]
+#![doc = include_str!("../README.md")]
 
 mod consts;
 
-pub use consts::*;
-
-use rand::Rng;
-use std::ops::Range;
-
-/// Converts a number to a base64 string.
+/// Converts a number to base64 in O(log(n)) time.
 ///
 /// # Examples
 ///
 /// ```
-/// use b64id::to_b64;
-/// use b64id::CHARS;
+/// use conv64::encode;
 ///
-/// for i in 0..=9 {
-///     assert_eq!(to_b64(i), i.to_string());
-/// }
-///
-/// for (i, c) in CHARS.iter().enumerate().skip(9) {
-///     assert_eq!(to_b64(i), c.to_string());
-/// }
+/// assert_eq!(encode(0), "0");
+/// assert_eq!(encode(1), "1");
+/// assert_eq!(encode(145), "2H");
 /// ```
-pub fn to_b64(num: usize) -> String {
-    let mut num = num;
-    let mut b64 = String::new();
+pub fn encode(mut num: usize) -> String {
+	if num < 64 {
+		return consts::CHARS[num].into();
+	}
 
-    if num == 0 {
-        return "0".into();
-    }
+	let estimated_length = num.next_power_of_two().trailing_zeros().max(1);
+	let mut b64 = String::with_capacity(estimated_length as usize);
 
-    while num > 0 {
-        b64 += &CHARS[(num & 63)].to_string();
-        num >>= 6;
-    }
+	while num > 0 {
+		// `num & 63`` is equivalent to `num % 64`,
+		// and `num >> 6` is equivalent to `num / 64`.
+		// Bitwise operations are usually faster, so we use them instead.
+		b64.insert(0, consts::CHARS[num & 63]);
+		num >>= 6;
+	}
 
-    b64.chars().rev().collect()
+	b64
 }
 
-/// Generates a random ID with range.
-pub fn generate(range: Range<usize>) -> String {
-    let mut rng = rand::thread_rng();
-    let num = rng.gen_range(range);
-
-    to_b64(num)
-}
-
-/// Gets the max value a base64 string with \<number_of_letters\> chars can represent.
+/// Generates a random base 64 number between a range of base 10 numbers.
 ///
-/// Returns 0 if the number of letters is bigger than 10. (if not checked and number is bigger than 10,
-/// it will panic with overflow)
-pub fn max_value_for_letters(number_of_letters: u8) -> usize {
-    if number_of_letters > 10 {
-        0
-    } else {
-        64_usize.pow(number_of_letters as u32)
-    }
+/// # Examples
+///
+/// ```
+/// use conv64::generate;
+///
+/// let id = generate(0..100);
+/// assert!(id.len() <= 2);
+/// ```
+#[cfg(feature = "rand")]
+pub fn generate(range: std::ops::Range<usize>) -> String {
+	use rand::Rng;
+
+	let mut rng = rand::thread_rng();
+	let num = rng.gen_range(range);
+
+	encode(num)
+}
+
+/// Gets the max value a base64 string with `n` chars can represent.
+///
+/// Equivalent to 64<sup>n</sup> - 1.
+///
+/// Returns `None` if the value is too big to fit in a `usize`.
+///
+/// # Examples
+/// ```
+/// use conv64::max_value_for_letters;
+///
+/// assert_eq!(max_value_for_letters(1), Some(63));
+/// assert_eq!(max_value_for_letters(2), Some(4095));
+/// ```
+pub fn max_value_for_letters(n: u32) -> Option<usize> {
+	64usize.checked_pow(n).map(|x| x - 1)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{generate, to_b64, CHARS};
+	use crate::{consts::CHARS, encode};
 
-    #[test]
-    fn test_to_b64() {
-        for i in 0..=9 {
-            assert_eq!(to_b64(i), i.to_string());
-        }
+	#[test]
+	fn test_to_b64() {
+		for i in 0..=9 {
+			assert_eq!(encode(i), i.to_string());
+		}
 
-        for (i, c) in CHARS.iter().enumerate().skip(9) {
-            assert_eq!(to_b64(i), c.to_string());
-            assert_eq!(to_b64(i).len(), 1);
-        }
+		for (i, c) in CHARS.iter().enumerate().skip(9) {
+			assert_eq!(encode(i), c.to_string());
+			assert_eq!(encode(i).len(), 1);
+		}
 
-        println!("{:?}", dbg!(1));
+		assert_eq!(encode(64), "10");
+		assert_eq!(encode(65), "11");
 
-        assert_eq!(to_b64(65), "11".to_string());
-        assert_eq!(to_b64(66), "12".to_string());
-        assert_eq!(to_b64(74), "1A".to_string());
-        assert_eq!(to_b64(75), "1B".to_string());
-        assert_eq!(to_b64(100), "1a".to_string());
-        assert_eq!(to_b64(126), "1-".to_string());
-        assert_eq!(to_b64(127), "1_".to_string());
-        assert_eq!(to_b64(128), "20".to_string());
-        assert_eq!(to_b64(164), "2a".to_string());
-    }
+		assert_eq!(encode(64 * 2), "20");
+		assert_eq!(encode(64 * 2 + 1), "21");
+	}
 
-    #[test]
-    fn test_gen() {
-        for i in 1000..2000 {
-            generate(i..usize::MAX);
-        }
-    }
+	#[test]
+	#[cfg(feature = "rand")]
+	fn generate() {
+		for _ in 0..100 {
+			let id = crate::generate(0..100);
+			assert!(id.len() <= 2);
+		}
+	}
 }
